3c900ebb15cde470d78351d3d93a877550bb7034
aswin <maswin.cool@gmail.com>
2016-01-05 10:20:02 +0530

    Implement reading menu option from menu view

--- src/com/twu/biblioteca/view/MenuView.java
+++ src/com/twu/biblioteca/view/MenuView.java
@@ -1,16 +1,23 @@
 package com.twu.biblioteca.view;
 
+import com.twu.biblioteca.InputOutput.InputReader;
 import com.twu.biblioteca.InputOutput.OutputWriter;
 
 public class MenuView {
     private OutputWriter outputWriter;
+    private InputReader inputReader;
 
-    public MenuView(OutputWriter outputWriter) {
+    public MenuView(OutputWriter outputWriter, InputReader inputReader) {
         this.outputWriter = outputWriter;
+        this.inputReader = inputReader;
     }
 
     public void displayMenu() {
         outputWriter.println("Menu Options :");
         outputWriter.println("1. List Books");
+    }
+
+    public int getMenuOption() {
+        return Integer.parseInt(inputReader.read());
     }
 }
--- test/com/twu/biblioteca/view/MenuViewTest.java
+++ test/com/twu/biblioteca/view/MenuViewTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca.view;
 
+import com.twu.biblioteca.InputOutput.InputReader;
 import com.twu.biblioteca.InputOutput.OutputWriter;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,18 +11,26 @@ import static org.mockito.Mockito.verify;
 public class MenuViewTest {
 
     private OutputWriter outputWriter;
+    private InputReader inputReader;
 
     @Before
     public void setUp() {
         outputWriter = Mockito.mock(OutputWriter.class);
+        inputReader = Mockito.mock(InputReader.class);
     }
 
     @Test
     public void shouldDisplayMenuOptions() throws Exception {
-        MenuView menuView = new MenuView(outputWriter);
+        MenuView menuView = new MenuView(outputWriter, inputReader);
         menuView.displayMenu();
         verify(outputWriter).println("Menu Options :");
         verify(outputWriter).println("1. List Books");
     }
 
+    @Test
+    public void shouldReadMenuOption() throws Exception {
+        MenuView menuView = new MenuView(outputWriter, inputReader);
+        menuView.getMenuOption();
+        verify(inputReader).read();
+    }
 }
